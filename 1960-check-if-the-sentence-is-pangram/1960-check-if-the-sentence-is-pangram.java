2a6fb3dd59510a4882b5f334487f5bd0df06e4ef
dazzy11 <150343655+dazzy11@users.noreply.github.com>
2024-11-04 11:53:22 +0530

    Time: 0 ms (100%), Space: 41.2 MB (89.48%) - LeetHub

---
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkIfPangram(String s) {
+        String a="abcdefghijklmnopqrstuvwxyz";
+        for(int i=0;i<a.length();i++){
+            if(!s.contains(String.valueOf(a.charAt(i)))){
+                return false;
+            }
+        }
+        return true;
+    }
+}
