6cc296db1efcca143dc12d045f1de244f56a0162
dazzy11 <150343655+dazzy11@users.noreply.github.com>
2024-11-05 12:06:29 +0530

    Time: 1 ms (87.28%), Space: 40.6 MB (85.05%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -2,12 +2,11 @@ class Solution {
     public int reverse(int x) {
         long rev=0;
         while(x!=0){
-            int rem=x%10;
-            rev=(rev*10)+rem;
+            rev=(rev*10)+(x%10);
             x=x/10;
+        }
         if(rev>=Integer.MAX_VALUE||rev<=Integer.MIN_VALUE)
                 return 0;
-        }
         return (int) rev;
     }
 }
