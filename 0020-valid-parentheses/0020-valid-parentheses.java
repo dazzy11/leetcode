3a975c50af1246282525f007eb10da9703584c20
dazzy11 <150343655+dazzy11@users.noreply.github.com>
2024-11-05 14:33:06 +0530

    Time: 3 ms (44.99%), Space: 41.5 MB (48.32%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c=='('||c=='{'||c=='['){
+                st.add(c);
+            }
+            else{
+                if(st.isEmpty()){
+                return false;
+            }
+            else if(c==')'&&st.peek()=='('){
+                st.pop();
+            }
+            else if(c=='}'&&st.peek()=='{'){
+                st.pop();
+            }
+            else if(c==']'&&st.peek()=='['){
+                st.pop();
+            }
+            else
+            return false;
+        }
+    }
+    if(st.isEmpty()){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+}
