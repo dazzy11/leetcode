9faabdc6ec65bb35ba37664ca3f7e4d2e8280878
dazzy11 <150343655+dazzy11@users.noreply.github.com>
2024-11-06 16:28:37 +0530

    Time: 8 ms (21.43%), Space: 44.6 MB (25.16%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.java
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.java
@@ -1,19 +1,15 @@
 class Solution {
     public boolean canSortArray(int[] nums) {
-        int[] arr=new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            count(nums[i],arr,i);
-        }
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
-                if(nums[i]>nums[j]&&arr[i]!=arr[j]){
+                if(nums[i]>nums[j]&&count(nums[i])!=count(nums[j])){
                     return false;
                 }
             }
         }
         return true;
     }
-    public static void count(int n,int[] arr,int i){
+    public static int count(int n){
         int count=0;
         while(n!=0){
             if((n&1)==1){
@@ -21,6 +17,6 @@ class Solution {
             }
             n=n>>1;
         }
-        arr[i]=count;
+        return count;
     }
 }
