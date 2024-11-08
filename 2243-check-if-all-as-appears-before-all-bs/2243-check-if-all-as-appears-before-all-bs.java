4894ba97adedcfdd66890e557bf7982fd092a0eb
dazzy11 <150343655+dazzy11@users.noreply.github.com>
2024-11-08 12:37:08 +0530

    Time: 0 ms (100%), Space: 41.5 MB (51.69%) - LeetHub

--- 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.java
+++ 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.java
@@ -1,10 +1,7 @@
 class Solution {
     public boolean checkString(String s) {
-        for(int i=s.length()-1;i>0;i--){
-            if(s.charAt(i)=='a'&&s.charAt(i-1)=='b'){
-                return false;
-            }
-        }
+        if(s.contains("ba"))
+            return false;
         return true;
     }
 }
