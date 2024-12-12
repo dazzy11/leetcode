0347bbe9e975155f40086a6829b8ded7b25e1e06
dazzy11 <150343655+dazzy11@users.noreply.github.com>
2024-12-13 00:25:56 +0530

    Time: 6 ms (65.07%), Space: 42.3 MB (66.23%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
@@ -2,17 +2,14 @@ class Solution {
     public long pickGifts(int[] g, int k) {
      PriorityQueue<Integer> q=new PriorityQueue<>(Comparator.reverseOrder());
      long sum=0;
-        for(int i=0;i<g.length;i++){
+        for(int i=0;i<g.length;i++)
             q.add(g[i]);
-        }
         for(int i=0;i<k;i++){
             int a=q.poll();
-            int n=(int)Math.floor(Math.sqrt(a));
-            q.add(n);
+            q.add((int)Math.sqrt(a));
         }
-        while(!q.isEmpty()){
-            sum=sum+q.poll();
-        }
+        while(!q.isEmpty())
+            sum+=q.poll();
         return sum;
     }
 }
