38bff31940e8bbb903a66a7381b5b81d0e1c727d
dazzy11 <150343655+dazzy11@users.noreply.github.com>
2024-12-15 15:38:45 +0530

    Time: 2120 ms (15.31%), Space: 40.3 MB (56.41%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,11 +1,10 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int sum=0;
+        int temp=num;
         for(int i=1;i<num;i++){
-            if(num%i==0){
-                sum+=i;
-            }
+            if(num%i==0)
+                temp-=i;
         }
-        return sum==num;
+        return temp==0;
     }
 }
