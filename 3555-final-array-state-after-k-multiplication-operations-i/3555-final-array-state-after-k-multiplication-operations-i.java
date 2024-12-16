b7bd226658f2a89706a373df6a8b2f483feb820b
dazzy11 <150343655+dazzy11@users.noreply.github.com>
2024-12-16 22:00:26 +0530

    Time: 2 ms (34.74%), Space: 45.2 MB (7.02%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
@@ -1,21 +1,15 @@
 class Solution {
     public int[] getFinalState(int[] nums, int k, int m) {
-        // HashMap<Integer,Integer> map= new Hashmap<>();
-        // PriorityQueue pq=new PriorityQueue<>();
-        // for(int i=0;i<nums.length;i++){
-        //     map.put(i,nums[i]);
-        //     pq.add(nums[i]);
-        // }
-        // for(int i=0;i<k;i++)
-
-        // }
-        int[] so=nums.clone();
-        for(int j=0;j<k;j++){
-            Arrays.sort(so);
-            for(int i=0;i<nums.length;i++){
-                if(nums[i]==so[0]){
-                    nums[i]=so[0]*m;
-                    so[0]=so[0]*m;
+        PriorityQueue<Integer> q=new PriorityQueue<>();
+        for(int i=0;i<nums.length;i++){
+            q.add(nums[i]);
+        }
+        for(int i=0;i<k;i++){
+            int a=q.poll();
+            for(int j=0;j<nums.length;j++){
+                if(a==nums[j]){
+                    nums[j]=a*m;
+                    q.add(nums[j]);
                     break;
                 }
             }
