047fa3dbec515bc6ac77f06d05f1deb9e41210ab
dazzy11 <150343655+dazzy11@users.noreply.github.com>
2024-12-19 22:59:07 +0530

    Time: 1 ms (38.31%), Space: 44 MB (5.61%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.java
+++ 3429-special-array-i/3429-special-array-i.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
-        for(int i=0;i<nums.length-1;i++){
-            if((nums[i]%2==0&&nums[i+1]%2==0)||(nums[i]%2!=0&&nums[i+1]%2!=0))
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1]%2==nums[i]%2)
                 return false;
         }
         return true;
