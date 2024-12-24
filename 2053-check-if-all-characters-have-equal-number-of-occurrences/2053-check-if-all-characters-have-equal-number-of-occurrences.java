e31fd90bd430136549adf2e54054f3c510e9b5d8
dazzy11 <150343655+dazzy11@users.noreply.github.com>
2024-12-24 23:50:47 +0530

    Time: 1 ms (100%), Space: 41.9 MB (76.14%) - LeetHub

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.java
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.java
@@ -1,21 +1,11 @@
 class Solution {
     public boolean areOccurrencesEqual(String s) {
         int[] arr=new int[26];
-        int z=0;
-        for(int i=0;i<s.length();i++){
-            int a=(int)s.charAt(i)-97;
-            arr[a]++;
-             z=arr[a];
-        }
+        for(char c:s.toCharArray())
+            arr[c-'a']++;
         for(int i=0;i<26;i++){
-            if(arr[i]==0){
-                continue;
-            }
-            else{
-                if(arr[i]!=z){
-                    return false;
-                }
-            }
+            if(arr[i]!=0&&arr[i]!=arr[s.charAt(0)-'a'])
+               return false;
         }
         return true;
     }
