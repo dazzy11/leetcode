33dc0ae08741745ba7a8123b4d203acab8afcc42
dazzy11 <150343655+dazzy11@users.noreply.github.com>
2025-02-06 14:09:08 +0530

    Time: 1 ms (39.48%), Space: 42 MB (58.08%) - LeetHub

--- 3321-type-of-triangle/3321-type-of-triangle.java
+++ 3321-type-of-triangle/3321-type-of-triangle.java
@@ -1,14 +1,11 @@
 class Solution {
     public String triangleType(int[] nums) {
-        if((nums[0]==nums[1])&&(nums[1]==nums[2])){
+        if((nums[0]==nums[1])&&(nums[1]==nums[2]))
             return "equilateral";
-        }
-        if(tri(nums)&&iso(nums)){
+        if(tri(nums)&&iso(nums))
             return "isosceles";
-        }
-        if(tri(nums)){
+        if(tri(nums))
             return "scalene";
-        }
         return "none";
     }
     public static boolean iso(int [] nums){
