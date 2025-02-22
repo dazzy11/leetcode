707f5dbb1a99e70480051718a8daeb83e7605337
dazzy11 <150343655+dazzy11@users.noreply.github.com>
2025-02-22 21:19:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean isPalindrome(int x) {
         int sum=0,temp=x;
-        if(x<0){
+        if(x<0)
             return false;
-        }
             while(x>0){
-                int rem=x%10;
-                sum=sum*10+rem;
+                sum=sum*10+(x%10)
                 x=x/10;
             }
             if(sum==temp)
