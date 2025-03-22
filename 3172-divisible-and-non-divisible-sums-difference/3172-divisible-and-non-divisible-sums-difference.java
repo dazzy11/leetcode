2e96b4c4d934e8702f976ae5d032c32a6268460c
dazzy11 <150343655+dazzy11@users.noreply.github.com>
2025-03-22 23:36:06 +0530

    Time: 1 ms (75.9%), Space: 40.6 MB (93.25%) - LeetHub

---
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int differenceOfSums(int n, int m) {
+        int sum=0;
+        for(int i=0;i<=n;i++){
+            if(i%m!=0)
+            sum=sum+i;
+            else
+            sum=sum-i;
+        }
+        return sum;
+    }
+}
