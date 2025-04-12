48e340d1d0030083cc455e82a2572d7e9b789ca7
dazzy11 <150343655+dazzy11@users.noreply.github.com>
2025-04-13 00:50:59 +0530

    Time: 1 ms (100%), Space: 44.6 MB (81.46%) - LeetHub

---
+++ 3582-find-indices-of-stable-mountains/3582-find-indices-of-stable-mountains.java
@@ -0,0 +1,8 @@
+class Solution {
+    public List<Integer> stableMountains(int[] h, int t) {
+        List<Integer> l=new ArrayList<>();
+        for(int i=1;i<h.length;i++)
+            if(h[i-1]>t) l.add(i);
+        return l;
+    }
+}
