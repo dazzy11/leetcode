7ac0b47e80bc97b197e1acf910ded65f90988e7e
Deepak Madhu Kumar N <150343655+dazzy11@users.noreply.github.com>
2025-07-18 23:29:59 +0530

    Time: 0 ms (100%), Space: 41 MB (5.25%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,10 +1,18 @@
 class Solution {
     public int climbStairs(int n) {
-       ArrayList<Integer> a=new ArrayList<>();
-       a.add(1);
-       a.add(1);
-       for(int i=0;i<n;i++)
-        a.add(a.get(i)+a.get(i+1));
-       return a.get(n);
+        if(n==0||n==1){
+            return 1;
+        }
+        int[] dp=new int[n+1];
+        int prev=1;
+        int curr=1;
+        int next=0;
+        for(int i=1;i<n;i++){
+            next=prev+curr;
+            prev=curr;
+            curr=next;
+        }
+        return curr;
+
     }
 }
