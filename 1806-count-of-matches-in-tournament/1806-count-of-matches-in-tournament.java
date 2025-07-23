d34e00fe344739bcccab731a1e233fb84cf143c6
Deepak Madhu Kumar N <150343655+dazzy11@users.noreply.github.com>
2025-07-23 14:55:57 +0530

    Time: 0 ms (100%), Space: 40.9 MB (18.51%) - LeetHub

--- 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.java
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.java
@@ -1,16 +1,5 @@
 class Solution {
     public int numberOfMatches(int n) {
-        int sum=0;
-        while(n>1){
-            if(n%2==0){
-                sum=(n/2)+sum;
-                n=n/2;
-            }
-            else{
-                sum=((n-1)/2)+sum;
-                n=((n-1)/2)+1;
-            }
-        }
-        return sum;
+       return n-1; 
     }
 }
