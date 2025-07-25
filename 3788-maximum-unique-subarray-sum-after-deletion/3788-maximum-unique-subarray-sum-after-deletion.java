d29c92316062b840dc652c48e49e4152f251aac4
Deepak Madhu Kumar N <150343655+dazzy11@users.noreply.github.com>
2025-07-25 23:06:16 +0530

    Time: 8 ms (3.13%), Space: 42.5 MB (50.31%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
@@ -1,10 +1,21 @@
 class Solution {
     public int maxSum(int[] nums) {
-        Arrays.sort(nums);
-        Set<Integer> s=new HashSet<>();
-        if(nums[nums.length-1]<0){
-            return nums[nums.length-1];
+        boolean check=false;
+        int n=-101;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                check=true;
+                break;
+            }
+            if(n<nums[i])
+            n=nums[i];
         }
+        System.out.println(n);
+        if(check==false){
+            return n;
+        }
+        Set<Integer> s=new HashSet<>();
+        
         for(int i=0;i<nums.length;i++){
             if(nums[i]>0){
         s.add(nums[i]);
