f2520b4cc45c50c3760aa588f014892efe7c54fc
Deepak Madhu Kumar N <150343655+dazzy11@users.noreply.github.com>
2025-07-28 12:47:08 +0530

    Time: 1 ms (18.74%), Space: 44.1 MB (51.16%) - LeetHub

--- 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.java
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.java
@@ -1,13 +1,17 @@
 class Solution {
     public int arraySign(int[] nums) {
-        int p=1;
+        int c=1;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==0)
-            return 0;
-            if(nums[i]>0) nums[i]=1; 
-            else nums[i]=-1;
-            p*=nums[i];
+            if(nums[i]==0){
+                return 0;
+            }
+            if(nums[i]>0){
+                c=c*1;
+            }
+            else{
+                c=c*-1;
+            }
         }
-        return p>0 ? 1:-1;
+        return c;
     }
 }
