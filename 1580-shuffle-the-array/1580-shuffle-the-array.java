2d5fb6992c10e40f7edcaa67467d703c09769a33
Deepak Madhu Kumar N <150343655+dazzy11@users.noreply.github.com>
2025-07-28 10:44:33 +0530

    Time: 1 ms (11.58%), Space: 44.7 MB (59.72%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.java
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -1,13 +1,15 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int [] arr=new int [2*n];
-        int i=0,j=0;
-        while(j<n){
-            arr[i]=nums[j];
-            arr[i+1]=nums[j+n];
-            j++;
-            i=i+2;
+        int base=10000;
+        for(int i=0;i<n;i++){
+            nums[i]=nums[i]+base*nums[n+i];
         }
-        return arr;
+        for(int i=n-1;i>=0;i--){
+            int x=nums[i]/base;
+            int y=nums[i]%base;
+            nums[i*2+1]=x;
+             nums[i*2]=y;
+        }
+        return nums;
     }
 }
