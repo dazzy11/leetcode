9ff287187083cdc7e696f0015568a8f5467527c5
Deepak Madhu Kumar N <150343655+dazzy11@users.noreply.github.com>
2025-10-22 19:17:12 +0530

    Time: 0 ms (100%), Space: 42.9 MB (36.11%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -1,12 +1,14 @@
 class Solution {
     public int finalValueAfterOperations(String[] o) {
-        int x=0;
+        int c=0;
         for(int i=0;i<o.length;i++){
-            if(o[i].contains("-"))
-                x=x-1;
-            else
-                x=x+1;
+            if(o[i].charAt(1)=='+'){
+                c++;
+            }
+            else{
+                c--;
+            }
         }
-        return x;
+        return c;
     }
 }
